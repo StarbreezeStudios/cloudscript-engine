a4a604d6a776bc98007d65dd18ec68fed7233a48
David Gaya <david.gaya@starbreeze.com>
2020-11-18 18:27:36 +0100

    migrate engine to commander action

--- src/commander.js
+++ src/commander.js
@@ -2,9 +2,20 @@
 'use strict';
 
 const local_require = require('./local_require');
-const secret = local_require('credentials.json');
-const upload = require('./uploader/upload');
+const secret = title => {
+  let secrets;
+  try {
+    secrets = local_require('credentials.json');
+  } catch(e) {
+    console.error('Local credentials.json file missing.');
+    process.exit(1);
+  }
+  return secrets[title];
+};
+
 const builder = require('./builder');
+const engine = require('./engine');
+const upload = require('./uploader');
 
 const { version } = require('../package.json');
 const { program } = require('commander');
@@ -26,13 +37,26 @@ program
   );
 
 program
-  .command('run <title> <source>', 'run cloudscript engine server', { executableFile: '../src/engine/index' });
+  .command('run <title> <source>')
+  .description('run cloudscript engine server')
+  .action((title, source) => {
+    const PORT = 3000;
+    const handlers = local_require(source);
+    const forward_url = `https://${title}.playfabapi.com`;
+    return engine({
+      title,
+      secret: secret(title),
+      port: PORT,
+      forward_url,
+      handlers
+    });
+  });
 
 program
   .command('upload <title> [bundle_path]')
   .description('upload built bundle to PlayFab')
   .action((title, bundle_path = 'dist/main.js') =>
-    report(upload(title, secret[title], bundle_path))
+    report(upload(title, secret(title), bundle_path))
   );
 
 program.parse(process.argv);
--- src/engine/index.js
+++ src/engine/index.js
@@ -1,7 +1,34 @@
 'use strict';
 
-const server = require('./server');
+const express = require('./custom_express');
 
-const config = require('./configuration_parser')();
+const index = ({title, secret, port, forward_url, handlers}) => {
 
-server(config);
+  const handlers_controller_injector = require('./handlers_controller_injector')(handlers);
+  const injector = handlers_controller_injector(title, secret);
+
+  const app = express();
+
+  app.post('/Client/ExecuteCloudScript', (req, res) => {
+    const session_ticket = req.headers['x-authorization'];
+    injector(session_ticket)
+      .execute_cloudscript(req.body)
+      .then(result => res.send(result))
+      .catch(err => {
+        console.error(err);
+        res.status(500).send('Internal server error. More information in cloudscript engine log.');
+      });
+  });
+
+  app.post('*', (req, res) => {
+    res.redirect(307, forward_url + req.url);
+    console.info('redirecting to', forward_url + req.url);
+  });
+
+  app.listen(port, () => {
+    console.info(`Cloudscript Engine running at http://localhost:${port}`);
+  });
+
+};
+
+module.exports = index;
