2458b83e30c8c2c552ebaf23fe885d8b2108511a
David Gaya <david.gaya@starbreeze.com>
2020-11-18 16:48:13 +0100

    clean code

--- src/commander.js
+++ src/commander.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const local_require = require('./local_require');
-const credentials = local_require('credentials.json');
+const secret = local_require('credentials.json');
 const upload = require('./uploader/upload');
 
 const { version } = require('../package.json');
@@ -15,17 +15,14 @@ program
   .command('run <title> <source>', 'run cloudscript engine server', { executableFile: '../src/engine/index' });
 
 program
-  .command('upload <title> [file_path]')
+  .command('upload <title> [bundle_path]')
   .description('upload built bundle to PlayFab')
-  .action((title, file_path) => {
-    file_path = file_path || 'dist/main.js';
-    const secret = credentials[title];
-    return upload(title, secret, file_path)
+  .action((title, bundle_path = 'dist/main.js') =>
+    upload(title, secret[title], bundle_path)
       .then(console.info)
       .catch(error => {
         console.error(error);
         process.exit(1);
-      });
-  });
+      }));
 
 program.parse(process.argv);
