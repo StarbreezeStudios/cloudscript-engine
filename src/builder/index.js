ac3a946cac665af8cf930b977e9a5efff5cfbaf0
Melvin Gutiérrez <melvin.gutierrez-rivero@gmail.com>
2020-11-23 09:03:06 +0000

    Remove template and underscore

--- src/builder/index.js
+++ src/builder/index.js
@@ -2,7 +2,6 @@
 const {dirname, basename, resolve} = require('path');
 const {promisify} = require('util');
 const webpack = promisify(require('webpack'));
-const create_local_wrapper = require('./create_local_wrapper');
 
 // This path is relative to cwd
 const WRAPPER_PATH = './tmp/entry.js';
@@ -12,13 +11,16 @@ module.exports = async (source, bundle_path) => {
   const config = {
     mode: 'none',
     entry: WRAPPER_PATH,
+    resolve: {
+      alias: {
+        Handlers: resolve(source)
+      }
+    },
     output: {
       path: resolve(dirname(bundle_path)),
       filename: basename(bundle_path)
     }
   };
-
-  create_local_wrapper(source, config.entry);
 
   return webpack(config)
     .then(stats => {
