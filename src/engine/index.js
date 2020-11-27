5cf36b9587ce41488f0795440c0ab491e2481225
David Gaya <david.gaya@starbreeze.com>
2020-11-27 19:08:43 +0100

    rename to forwarder

--- src/engine/index.js
+++ src/engine/index.js
@@ -4,20 +4,20 @@ const local_require = require('../local_require');
 const web_server = require('./web_server');
 const Inspect = require('./inspect');
 const Monitor = require('./monitor');
-const forward_methods = require('./forward_methods');
+const forwarders = require('./forwarders');
 const validators = require('./validators');
 
 const run = (source, {title, secret, port, monitor, inspect, forwardMethod}) => {
-  const forward_method = forward_methods[forwardMethod];
+  const forwarder = forwarders[forwardMethod];
 
   const handlers = local_require(source);
 
-  return web_server(forward_method)({title, port, secret, handlers})
+  return web_server(forwarder)({title, port, secret, handlers})
     .then(() => monitor && Monitor(source))
     .then(() => inspect && Inspect());
 };
 
 module.exports = {
-  validators: validators(forward_methods),
+  validators: validators(forwarders),
   run
 };
