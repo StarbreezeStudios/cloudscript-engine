a2a0557785ca7820836d3e9b32d1da9271ca3cf2
Melvin Gutiérrez <melvin.gutierrez-rivero@gmail.com>
2020-12-01 11:47:13 +0000

    Add bunyan logger

--- src/engine/web_server/custom_express.js
+++ src/engine/web_server/custom_express.js
@@ -2,9 +2,24 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const express_bunyan_logger = require('express-bunyan-logger');
+
+const build_logger_options = () => {
+  const timestamp = Date.now();
+  return {
+    name: timestamp.toString(),
+    streams: [{
+      path: `logs/${timestamp}.log`,
+    }]
+  };
+};
 
 module.exports = options => {
   const app = express(options);
+  const logger_options = build_logger_options();
+
   app.use(bodyParser.json());
+  app.use(express_bunyan_logger(logger_options));
+
   return app;
 };
